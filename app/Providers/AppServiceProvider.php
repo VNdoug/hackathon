203eb5debe1f302f64c4ecb9e5f834addcb84a9d
lffruhling <lffruhling@hotmail.com>
2018-09-28 20:37:58 -0300

    Adiciona ajuste para hora local e erro de emai no mysql 5.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /* Utilizado para MySql 5.7.7 onde da erro em campos unique*/
+        Schema::defaultStringLength(191);
+
+        setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
     }
 
     /**
