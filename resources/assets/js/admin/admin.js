9861ea7d2655ddffbaa263e4d2ad486fa00ddb09
Douglas Vinícius <douglas.vn01@gmail.com>
2018-09-29 03:24:13 -0300

    Adiciona script de calendario de medicos

--- resources/assets/js/admin/admin.js
+++ resources/assets/js/admin/admin.js
@@ -9,15 +9,59 @@ $('#calendar').fullCalendar({
     locale: 'pt-br',
     timeFormat: 'HH:mm',
 
-    eventSources: [
-        {
-          url: '/ajax/atendente/agendamentos',
-          type: 'GET',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,listMonth,listWeek,listDay'
+    },
+    
+    buttonText: {
+      month: 'Mês',
+      listMonth: 'Mês (lista)',
+      listWeek: 'Semana',
+      listDay: 'Dia'
+    },
 
-          error: function() {
-            alert('Erro ao obter agendamentos!');
-          },
-          
-        }
-      ]
+    eventSources: [
+      {
+        url: '/ajax/atendente/agendamentos',
+        type: 'GET',
+
+        error: function() {
+          alert('Erro ao obter agendamentos!');
+        },
+        
+      }
+    ]
+});
+
+$('#calendar-medico').fullCalendar({
+    defaultView: 'listDay',
+    locale: 'pt-br',
+    timeFormat: 'HH:mm',
+
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,listMonth,listWeek,listDay'
+    },
+    
+    buttonText: {
+      month: 'Mês',
+      listMonth: 'Mês (lista)',
+      listWeek: 'Semana',
+      listDay: 'Dia'
+    },
+
+    eventSources: [
+      {
+        url: '/ajax/medico/agendamentos',
+        type: 'GET',
+
+        error: function() {
+          alert('Erro ao obter agendamentos!');
+        },
+        
+      }
+    ]
 });
