2cc478d9d75ca8e502b69b162fd716973c47a34d
lffruhling <lffruhling@hotmail.com>
2018-09-28 22:40:23 -0300

    adiciona mensagens

---
+++ resources/views/vendor/adminlte/partials/messages.blade.php
@@ -0,0 +1,24 @@
+@if ($errors->any())
+	<ul class="alert alert-error alert-dismissable list-none">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		@foreach ($errors->all() as $error)
+			<li><i class="fa fa-exclamation-circle"></i> {{ $error }}</li>
+		@endforeach
+	</ul>
+@endif
+
+@if(Session::get('success'))
+	<div class="alert alert-success alert-dismissable">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<i class="icon fa fa-check"></i>
+		{{Session::get('success')}}
+	</div>
+@endif
+
+@if(Session::get('warning'))
+	<div class="alert alert-warning alert-dismissible">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<i class="icon fa fa-warning"></i>
+		{{Session::get('success')}}
+	</div>
+@endif
