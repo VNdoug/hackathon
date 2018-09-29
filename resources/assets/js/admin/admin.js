2e1bd0a6f701640858a672246e2d03afe5e65d5e
lffruhling <lffruhling@hotmail.com>
2018-09-29 03:07:15 -0300

    Adiciona puglins de data e hora

--- resources/assets/js/admin/admin.js
+++ resources/assets/js/admin/admin.js
@@ -1,6 +1,16 @@
 import $ from 'jquery';
 import 'fullcalendar' ;
+import 'bootstrap-datepicker';
 
 $('#calendar').fullCalendar({
     // put your options and callbacks here
+});
+
+//Date picker
+$('#datepicker').datepicker({
+    autoclose: true,
+    daysOfWeekDisabled: [0,6],
+    format: "dd/mm/yyyy",
+    language:"pt-BR",
+    todayHighlight:true,
 });
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -15,6 +15,8 @@
     <!-- Ionicons -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/Ionicons/css/ionicons.min.css') }}">
 
+    <!-- Bootstrap time Picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/timepicker/bootstrap-timepicker.min.css') }}">
     @if(config('adminlte.plugins.select2'))
         <!-- Select2 -->
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">
@@ -63,7 +65,20 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
 @endif
 
+<script src="{{ asset("plugins/timepicker/bootstrap-timepicker.min.js") }}"></script>
+
 @yield('adminlte_js')
+
+<script>
+    //Timepicker
+     $('.timepicker').timepicker({
+         showInputs: false,
+         showSeconds:false,
+         defaultTime:false,
+         showMeridian:false,
+         maxHours:18,
+     });
+</script>
 
 </body>
 </html>
