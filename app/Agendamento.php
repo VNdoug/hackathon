9b4f6e456c9d2dae3b47cc5fe6171b93e4c15eb0
Douglas Vinícius <douglas.vn01@gmail.com>
2018-09-29 02:39:03 -0300

    Adiciona relationships para agendamento

--- app/Agendamento.php
+++ app/Agendamento.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Agendamento extends Model
 {
@@ -10,4 +11,27 @@ class Agendamento extends Model
         'paciente_id','medico_id','data_pre_agendamento','data_consulta','sintomas','receita','avaliacao_medica',
         'observacoes','confirmada','concluida'
     ];
+
+
+    /* Scopes */
+    public function scopeComRelacionamentos($query) {
+        return $query->with(['paciente', 'medico']);
+    }
+
+
+    /* Relationships */
+    public function paciente() {
+        return $this->belongsTo(User::class, 'paciente_id');
+    }
+
+    public function medico() {
+        return $this->belongsTo(User::class, 'medico_id');
+    }
+
+
+    /* Getters */
+    public function getCorCalendarioAttribute() {
+        return 'red';
+    }
+
 }
